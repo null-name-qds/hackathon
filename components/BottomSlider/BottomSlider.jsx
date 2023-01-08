f8adc97e9fe5c01495aafaa1c01bb65ef999abb4
Ishan-sa <ishansach19@gmail.com>
2023-01-07 18:59:34 -0800

    started bottom bar

---
+++ components/BottomSlider/BottomSlider.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { MdOutlineArrowUpward } from "react-icons/md";
+import Chart from "../Chart/Chart";
+
+export default function BottomSlider() {
+  return (
+    <>
+      <div className="w-full bg-[#F3F3FE] py-4 fixed z-[999] bottom-[-150px] min-h-[200px]">
+        <MdOutlineArrowUpward className="mx-auto" />
+        <div className="max-w-[200px]">
+            <Chart />
+        </div>
+      </div>
+    </>
+  );
+}

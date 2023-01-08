cfd6838f7d3037d47629c88730283095921a718f
Ishan-sa <ishansach19@gmail.com>
2023-01-07 16:47:18 -0800

    doing charts

--- components/Sidebar/Sidebar.jsx
+++ components/Sidebar/Sidebar.jsx
@@ -1,12 +1,16 @@
+import Image from "next/image";
 import Chart from "../Chart/Chart";
-
 
 export default function Sidebar() {
     return (
         <>
-            <div className='h-screen max-w-[300px]'>
-                <h1>hey</h1>
-                <Chart />
+            <div className='h-screen bg-[#F3F3FE] px-4 py-2 flex flex-row'>
+                {/* <div className="flex"> */}
+                    <Chart />
+                {/* </div> */}
+                {/* <div className="flex">
+                    <Image src={'./filter-icon.svg'} width={40} height={40} />
+                </div> */}
             </div>
         </>
     )

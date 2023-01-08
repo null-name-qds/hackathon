9c1d7b59a97cd5a8813b6fa5c17c4e07baf2bdd2
ssn00zee <cjavillo@my.bcit.ca>
2023-01-07 19:12:08 -0800

    partial popup style

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -5,8 +5,39 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css"; // Re-uses images from ~leaflet package
 import "leaflet-defaulticon-compatibility";
 import {BsHouseFill} from "react-icons/bs";
+import styled from 'styled-components'
 
-export default function Map({ results }) {
+const NewPop = styled(Popup)`
+  border-radius: 0;
+  .leaflet-popup-content-wrapper {
+    box-shadow: 8px 8px 0px #7E6DE7;
+    border-radius: 7.5px;
+    background-color: #ffffff;
+    width: 150px;
+    height: 150px;
+    border: solid 1px #7E6DE7;
+  }
+  
+  .leaflet-popup-tip-container {
+    visibility: hidden;
+  }
+
+  .leaflet-popup-close-button > span {
+    background-color: #7E6DE7;
+    border-radius: 50%;
+    height: 12px;
+    width: 12px;
+    line-height: 12px;
+    padding-left: 4px;
+    padding-right: 4px;
+    text-align: center;
+    display: inline-block;
+  }
+`
+
+export default function Map({ 
+  results
+}) {
   const purpleOption = { color: "purple" };
   const redOption = { color: "red" };
   return (
@@ -20,16 +51,21 @@ export default function Map({ results }) {
               <Marker
                   key={result.Id}
                   position={[result.Property.Address.Latitude, result.Property.Address.Longitude]}
+                  eventHandlers={{
+                    click: () => {
+                      console.log(result.Id)
+                    }
+                  }}
               >
-                  <Popup>
+                  <NewPop>
                       {result.Property.Address.AddressText} <br/>
                       {result.Property.Price}
-                  </Popup>
+                  </NewPop>
               </Marker>
           )) : (<Marker position={[49.2577302,-123.1589232]}>
-              <Popup>
+              <NewPop>
                   A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
+              </NewPop>
           </Marker>)}
       </MapContainer>
     </>
